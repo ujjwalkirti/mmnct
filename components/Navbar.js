89d25dbc34fd3f2d3fb7c13068f3a36d714c5798
Ujjwal Kirti <kirti.chargers@gmail.com>
2022-01-12 11:24:45 +0530

    12-1-2022

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,21 +2,32 @@ import { FaPollH } from "react-icons/fa";
 function Navbar() {
   return (
     <div className="text-white bg-gradient-to-r from-blue-900 via-blue-700 to-blue-900 h-16 flex px-10  justify-between">
-      <img src="main.png" className="rounded-md m-2 shadow-lg" />
+      <a className="h-full items-center flex" href="/">
+        <img src="main.png" className="rounded-md mx-2 shadow-lg h-14" />
+      </a>
       <div className="flex items-center w-3/5 justify-around text-lg font-semibold">
-        <p className="cursor-pointer ">MATCHES</p>
-        <p className="cursor-pointer ">STATS</p>
-        <p className="cursor-pointer ">POINTS TABLE</p>
-        <p className="cursor-pointer ">TEAMS</p>
-        <p className="cursor-pointer ">NEWS</p>
+        <a href="/matches" className="cursor-pointer ">
+          MATCHES
+        </a>
+        {/* <a href="/stats" className="cursor-pointer ">STATS</a> */}
+        <a href="/stats" className="cursor-pointer ">
+          POINTS TABLE
+        </a>
+        <a href="/teams" className="cursor-pointer ">
+          TEAMS
+        </a>
+        {/* <a className="cursor-pointer "></a> */}
       </div>
-      <div className="flex items-center text-2xl font-bold cursor-pointer text-yellow-500">
-        <FaPollH  className="mx-2"/>
+      <a
+        href="/polls"
+        className="flex items-center text-2xl font-bold cursor-pointer text-yellow-500"
+      >
+        <FaPollH className="mx-2" />
         Polls
         {/* <span className=" text-2xl font-bold bg-gradient-to-r from-red-500 via-yellow-500 to-red-400">
           <p className="text-transparent">Polls</p>
         </span> */}
-      </div>
+      </a>
     </div>
   );
 }
