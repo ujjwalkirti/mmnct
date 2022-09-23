7a562f0ef98c499c48d86ba3ee29659dc7b714ac
ujjwalkirti <kirti.chargers@gmail.com>
2022-09-23 17:03:54 +0530

    23-09-2022

---
+++ components/Home.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import { SiGooglemaps } from "react-icons/si";
+function HomePage() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen font-extrabold">
+      <p className="text-5xl">Manoj Memorial</p>
+      <p className="text-2xl">Night Cricket Tournament</p>
+
+      <Image src="/vector-1.jpg" width={300} height={300} />
+      <div className="text-center flex items-center">
+        <SiGooglemaps className="text-3xl mr-4"/>
+        <div>
+          <p className="text-xl">SVNIT, Surat</p>
+          <p>SAC Ground</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
