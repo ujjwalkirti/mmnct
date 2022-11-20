cc3254c65315e1265d10cf9d9469cc8f144c55e9
ujjwalkirti <kirti.chargers@gmail.com>
2022-11-21 02:29:49 +0530

    changed the ui

--- pages/organisers.js
+++ pages/organisers.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
 import Navbar from "../components/Navbar";
 
@@ -9,7 +10,24 @@ function Organisers() {
         <title>Organising Team</title>
       </Head>
       <Navbar />
-      
+      <div className="text-center">
+        <p>
+          You must be wondering, who all are behind organising such a humongous
+          event successfully!
+        </p>
+        <p>Well, your wait is over!</p>
+        <p>Presenting before you, the organising committee!</p>
+        {/* <div className="relative">
+          <Image src="/vector-7.png" height={300} width={500} />
+          <p className="absolute top-14  px-2 py-4 font-bold text-3xl text-white">Student community of SVNIT!</p>
+        </div> */}
+
+        <p>
+          Yes you read it correctly, all the students irrespective of there
+          branch and year cooperated and collaborated to help make this event a
+          reality!
+        </p>
+      </div>
     </div>
   );
 }
