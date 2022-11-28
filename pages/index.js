3eb8fdddd458256df54fb79d1a2ebb44e47962cb
unknown <sk662516@gmail.com>
2022-11-29 03:10:21 +0530

    Modified the memory file

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      
+
       <HomePage />
       <Followup />
 
-      
       <Footer />
     </div>
   );
--- pages/memories.js
+++ pages/memories.js
@@ -1,22 +1,59 @@
 import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { app } from "../components/db/Firebase";
+import { getStorage, getDownloadURL, listAll, ref } from "firebase/storage";
 
 function Memories() {
+  const storage = getStorage(app);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "memories/");
+
+  useEffect(() => {
+    listAll(imageListRef).then((res) => {
+      res.items.forEach((itemRef) => {
+        getDownloadURL(itemRef).then((url) => {
+          setImageList((imageList) => [...imageList, url]);
+        });
+      });
+    });
+  }, []);
+
   return (
-    <div className="text-xl text-center">
-      <Head>
-        <title>Memories from the past versions!</title>
-      </Head>
+    <>
       <Navbar />
-      <p className="text-3xl font-bold text-center">Memories</p>
-      <div className="mx-auto w-3/5">
-        <Image src="/vector-4.jpg" width={200} height={200} />
+      <div className="bg-black">
+        <div className="flex text-white text-center h-72 bg-gray-900 bg-opacity-40">
+          <div className="m-auto">
+            <p className="text-4xl font-bold">Memories</p>
+            <p className="mt-8 text-2xl font-light">
+              Have a look how spectacle the past versions were!
+            </p>
+          </div>
+        </div>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            <img
+              src="https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg"
+              className="m-3 rounded-lg"
+            />
+            <img
+              src="https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg"
+              className="m-3 rounded-lg"
+            />
+            <img
+              src="https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg"
+              className="m-3 rounded-lg"
+            />
+            {imageList.map((url) => {
+              return <img src={url} className="m-3 rounded-lg" />;
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
-      <p className="mx-2">Have a look how spectacle the past versions were!</p>
-      
-    </div>
+    </>
   );
 }
 
