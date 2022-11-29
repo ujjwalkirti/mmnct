9f3bba41ed1ec8bc25a4b649b8d3d91c40c0c7e5
Ujjwal Kirti <kirti.chargers@gmail.com>
2022-11-29 10:34:13 +0530

    changed the navbar look

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { VscCloseAll } from "react-icons/vsc";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+
 const options = [
   { name: "Fixtures", url: "/fixtures" },
   { name: "Points Table", url: "/points-table" },
@@ -12,24 +15,32 @@ const options = [
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className="p-2 relative flex justify-end">
+    <div className="p-2 relative flex justify-end bg-black text-white">
       {/* <div className="flex items-center font-bold">
         <Image src="/main.png" height={80} width={100} alt="logo" />
         <p className="ml-2 text-3xl">MMNCT</p>
       </div> */}
-      <Image
-        src="/ham.png"
-        height={30}
-        width={30}
-        layout="fixed"
-        alt="Man playing cricket"
-        className="hover:shadow-lg  cursor-pointer"
-        onClick={() => {
-          setShowMenu(true);
-        }}
-      />
+      <div className="hover:shadow-lg cursor-pointer flex justify-between w-full items-center px-5 mb-5 md:hidden">
+        <p className="text-green-600 mt-5 font-bold text-2xl">MMNCT</p>
+        {/* <Image
+          src="/ham.png"
+          height={30}
+          width={30}
+          layout="fixed"
+          alt="Man playing cricket"
+          onClick={() => {
+            setShowMenu(true);
+          }}
+        /> */}
+        <GiHamburgerMenu
+          className="mt-5 hover:text-green-600 text-2xl"
+          onClick={() => {
+            setShowMenu(true);
+          }}
+        />
+      </div>
       {showMenu && (
-        <div className="absolute flex flex-col w-full md:w-2/5 z-40 xl:w-1/5 top-0 right-0 bg-white px-4 py-2">
+        <div className="absolute flex flex-col w-full md:w-2/5 z-40 xl:w-1/5 top-0 right-0 bg-white text-black px-4 py-2">
           <VscCloseAll
             className="font-semibold text-2xl absolute top-0 right-0 m-4 hover:cursor-pointer"
             onClick={() => {
@@ -46,6 +57,20 @@ function Navbar() {
           ))}
         </div>
       )}
+
+      {/* content to be shown when screen size is medium and above */}
+      <div className="w-full md:flex font-semibold text-3xl justify-between hidden ">
+        <div>
+          <p className="text-green-700">MMNCT</p>
+        </div>
+        <div className="">
+          {options.map((option) => (
+            <a className="my-4  px-4 py-2" href={option.url}>
+              {option.name}
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+import "../styles/Banner.css";
+import "../styles/GlowingButton.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
