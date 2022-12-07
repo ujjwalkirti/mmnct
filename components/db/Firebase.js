b307fb9ab0712536d38ffa72569243b38cd07d6a
ujjwalkirti <kirti.chargers@gmail.com>
2022-12-08 02:32:37 +0530

    provided ui improvement and available for both mobile and other breakpoints

--- components/db/Firebase.js
+++ components/db/Firebase.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics, isSupported } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore, initializeFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -23,6 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+
+
 export { db, storage };
 
 // const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
