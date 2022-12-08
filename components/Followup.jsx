38055b21698b8507fc992fed6378ebb28a275362
ujjwalkirti <kirti.chargers@gmail.com>
2022-12-09 01:57:33 +0530

    improved the ui of home page

--- components/Followup.jsx
+++ components/Followup.jsx
@@ -53,9 +53,6 @@ const options = [
   },
 ];
 
-const description =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
-
 const Followup = () => {
   return (
     <div className="text-center">
@@ -82,20 +79,14 @@ const Followup = () => {
           );
         })}
       </div>
+
+      {/* ask for suggestions from viewers */}
+      <div></div>
+
+      
       {/* Details */}
     </div>
   );
 };
-
-//fix this function and make it fetch the data from firebase
-export async function getServerSideProps(context) {
-  // Fetch data from external API
-
-  const description =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
-
-  // Pass data to the page via props
-  return { props: description };
-}
 
 export default Followup;
