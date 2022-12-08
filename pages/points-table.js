3add7d6bf14dc200f2737a1893e20a64f8a2be63
sk662516 <sk662516@gmail.com>
2022-12-09 02:33:10 +0530

    Coming soon on point table page

--- pages/points-table.js
+++ pages/points-table.js
@@ -1,17 +1,24 @@
-import Head from 'next/head';
-import React from 'react'
-import Navbar from '../components/Navbar'
+import Head from "next/head";
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 function PointsTable() {
   return (
     <div>
-      
       <Head>
         <title>Points-Table</title>
       </Head>
       <Navbar />
+      <div className="min-h-screen">
+        <div className="flex items-center justify-center text-center">
+          <p className="mt-80 text-3xl font-semibold">Coming soon...</p>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
 
-export default PointsTable
+export default PointsTable;
