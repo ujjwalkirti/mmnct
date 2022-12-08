0b4ffc0f961351d344140ba2103e5fe4511bb27f
prince-codes23 <84957374+prince-codes23@users.noreply.github.com>
2022-12-09 02:51:20 +0530

    Improved the page for Points Table

--- pages/points-table.js
+++ pages/points-table.js
@@ -2,21 +2,32 @@ import Head from "next/head";
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import styles from "../styles/Match.module.css";
 
 function PointsTable() {
   return (
-    <div>
+        <div>
       <Head>
         <title>Points-Table</title>
       </Head>
       <Navbar />
-      <div className="min-h-screen">
-        <div className="flex items-center justify-center text-center">
-          <p className="mt-80 text-3xl font-semibold">Coming soon...</p>
-        </div>
+      <div className={styles.pointtablemain}>
+        <nav className={styles.navbar1}>
+          <div>
+            <button
+            className={styles.btn_group__item}
+            >
+              Male PointsTable
+            </button>
+            <button
+            className={styles.btn_group__item}
+            >
+              Female PointsTable
+            </button>
+          </div>
+        </nav>
+        <h1 className={styles.cs}>Coming Soon....</h1>
       </div>
-
-      <Footer />
     </div>
   );
 }
