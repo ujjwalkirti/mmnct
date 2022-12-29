e479e105237b2c0c07b456738405c90a7ea04c94
sk662516 <sk662516@gmail.com>
2022-12-29 15:58:57 +0530

    Add team details admin page

---
+++ components/Modal.jsx
@@ -0,0 +1,192 @@
+import Image from "next/image";
+import React from "react";
+import { FiEdit } from "react-icons/fi";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+import { db, storage } from "./db/Firebase";
+import { setDoc, deleteDoc, doc } from "firebase/firestore";
+
+export default function Modal({ details }) {
+  const [showModal, setShowModal] = React.useState(false);
+
+  const updateTeamDetails = async (e) => {
+    e.preventDefault();
+    // Get file from input
+    let file = e.target[0].files[0];
+    //Get Team name from input
+    let teamName = e.target[1].value;
+    //Get Team type from input
+    let teamType = e.target[2].value;
+
+    const metadata = {
+      contentType: "image/jpeg",
+    };
+
+    if (teamName == "") {
+      teamName = details.teamName;
+    }
+
+    if (teamType == "") {
+      teamType = details.teamType;
+    }
+
+    let storageRef = ref(storage, `teams_logo/${teamName}.jpg`);
+
+    if (file != null) {
+      await uploadBytes(storageRef, file, metadata).then(
+        async (snapshot) => {
+          console.log("Uploaded the file!");
+          await getDownloadURL(storageRef).then(async (downloadURL) => {
+            details.teamLogo = downloadURL;
+          });
+
+          if (teamName != details.teamName) {
+            // Create a reference to the file to delete
+            const desertRef = ref(
+              storage,
+              `teams_logo/${details.teamName}.jpg`
+            );
+
+            // Delete the file
+            await deleteObject(desertRef)
+              .then(() => {
+                console.log("File deleted successfully");
+              })
+              .catch((error) => {
+                alert(error);
+              });
+          }
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+    }
+
+    if (teamName != details.teamName) {
+      // Delete the old document and create a new one
+      await deleteDoc(doc(db, "participating-teams", details.teamName));
+    }
+    await setDoc(doc(db, "participating-teams", teamName), {
+      teamName: teamName,
+      teamType: teamType,
+      teamLogo: details.teamLogo,
+    });
+    alert("Team details updated successfully");
+    setShowModal(false);
+    location.reload();
+  };
+
+  return (
+    <>
+      <button type="button" onClick={() => setShowModal(true)}>
+        <FiEdit />
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="p-5 border-b border-solid border-slate-200 rounded-t">
+                  <p className="text-xl font-semibold text-center">
+                    Update details
+                  </p>
+                </div>
+                {/*body*/}
+
+                <form
+                  class="w-full max-w-sm mx-auto px-4 py-6"
+                  onSubmit={updateTeamDetails}
+                >
+                  <div class="mb-6 ml-2 md:ml-6">
+                    <Image
+                      src={details.teamLogo}
+                      width={300}
+                      height={300}
+                      className="border"
+                    />
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label
+                        className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                        for="file"
+                      >
+                        Team Logo
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input id="file" type="file" />
+                    </div>
+                  </div>
+
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label
+                        className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                        for="team_name"
+                      >
+                        Team Name
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="team_name"
+                        type="text"
+                        placeholder={details.teamName}
+                      />
+                    </div>
+                  </div>
+
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label
+                        className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                        for="team_type"
+                      >
+                        Team Type
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input
+                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="team_type"
+                        type="text"
+                        placeholder={details.teamType}
+                      />
+                    </div>
+                  </div>
+
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                    <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="submit"
+                    >
+                      Save Changes
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- pages/admin/addTeams.js
+++ pages/admin/addTeams.js
@@ -1,9 +1,267 @@
-import React from 'react'
+import Head from "next/head";
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { db, storage } from "../../components/db/Firebase";
+import {
+  collection,
+  setDoc,
+  updateDoc,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
-const AddTeams = () => {
-  return (
-    <div>AddTeams</div>
-  )
+import Image from "next/image";
+import Modal from "../../components/Modal";
+
+// Next js getserversideprops for getting the data from the database
+export async function getServerSideProps() {
+  const teamsRef = collection(db, "participating-teams");
+  const teamsSnap = await getDocs(teamsRef);
+  const teams = teamsSnap.docs.map((doc) => doc.data());
+
+  return {
+    props: {
+      teams: teams,
+    },
+  };
 }
 
-export default AddTeams
+const AddTeams = ({ teams }) => {
+  const [loading, setLoading] = useState(false);
+  const [newTeam, setNewTeam] = useState(false);
+  const [updateTeam, setUpdateTeam] = useState(false);
+
+  const addTeamDetails = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // Get file from input
+    const file = e.target[0].files[0];
+    //Get Team name from input
+    const teamName = e.target[1].value;
+    //Get Team type from input
+    const teamType = e.target[2].value;
+
+    let storageRef = ref(storage, `teams_logo/${teamName}.jpg`);
+
+    const metadata = {
+      contentType: "image/jpeg",
+    };
+
+    //Check if the team already exists
+    const docRef = doc(db, "participating-teams", teamName);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      alert("Team already exists");
+      setLoading(false);
+    } else {
+      //Upload image to firebase storage
+      uploadBytes(storageRef, file, metadata).then(
+        (snapshot) => {
+          console.log("Uploaded the file!");
+
+          //Get the download url of the image
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            // console.log(downloadURL);
+            //Add the team name and download url to the database
+            await setDoc(doc(db, "participating-teams", teamName), {
+              teamName: teamName,
+              teamType: teamType,
+              teamLogo: downloadURL,
+            });
+            alert("Team added successfully");
+            setLoading(false);
+            location.reload();
+          });
+        },
+        (error) => {
+          console.log(error);
+          setLoading(false);
+        }
+      );
+    }
+  };
+  return (
+    <div>
+      <Head>
+        <title>Team details update</title>
+      </Head>
+      <Navbar />
+      <div className="flex justify-center items-center mt-8">
+        <button
+          className="border border-black py-2 px-4 my-2"
+          onClick={() => {
+            setNewTeam(true);
+            setUpdateTeam(false);
+          }}
+        >
+          Add new Team
+        </button>
+        <button
+          className="border border-black py-2 px-4 my-2"
+          onClick={() => {
+            setNewTeam(false);
+            setUpdateTeam(true);
+          }}
+        >
+          Update existing Team
+        </button>
+      </div>
+
+      {!newTeam && !updateTeam && (
+        <div>
+          <p className="text-center my-10 text-3xl">Existing Team details</p>
+          <div className="flex justify-center text-center px-1">
+            <div className="text-sm md:text-base">
+              <table className="table-auto">
+                <thead className="border-b bg-gray-800 text-white">
+                  <tr>
+                    <th className="px-4 md:px-10 py-2">Team Logo</th>
+                    <th className="px-4 md:px-10 py-2">Team Name</th>
+                    <th className="px-4 md:px-10 py-2">Team Type</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {teams.map((team, index) => (
+                    <tr className="border-b" key={index}>
+                      <td className="px-4 py-2">
+                        <Image
+                          src={team.teamLogo}
+                          width={100}
+                          height={100}
+                          className="border"
+                          alt="Team Logo"
+                        />
+                      </td>
+                      <td className="px-4 py-auto">{team.teamName}</td>
+                      <td className="px-4 py-auto">{team.teamType}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {newTeam && (
+        <div>
+          <p className="text-center my-10 text-3xl">Add Team details</p>
+
+          <form class="w-full max-w-sm mx-auto px-4" onSubmit={addTeamDetails}>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="file"
+                >
+                  Team Logo
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input id="file" type="file" required />
+              </div>
+            </div>
+
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="team_name"
+                >
+                  Team Name
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="team_name"
+                  type="text"
+                  required
+                />
+              </div>
+            </div>
+
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="team_type"
+                >
+                  Team Type
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="team_type"
+                  type="text"
+                  required
+                />
+              </div>
+            </div>
+
+            <div class="md:flex md:items-center">
+              <div class="md:w-1/3"></div>
+              <div class="md:w-2/3">
+                <button
+                  class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="submit"
+                >
+                  Add Team
+                </button>
+              </div>
+            </div>
+          </form>
+
+          {loading && (
+            <p className="text-center mt-10 text-2xl">Please wait.....</p>
+          )}
+        </div>
+      )}
+      {updateTeam && (
+        <div>
+          <p className="text-center my-10 text-3xl">Update Team details</p>
+          <div className="flex justify-center text-center px-1">
+            <div className="text-sm md:text-base">
+              <table className="table-auto">
+                <thead className="border-b bg-gray-800 text-white">
+                  <tr>
+                    <th className="px-4 md:px-10 py-2">Team Logo</th>
+                    <th className="px-4 md:px-10 py-2">Team Name</th>
+                    <th className="px-4 md:px-10 py-2">Team Type</th>
+                    <th className="px-4 md:px-10 py-2">Update</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {teams.map((team, index) => (
+                    <tr className="border-b" key={index}>
+                      <td className="px-4 py-2">
+                        <Image
+                          src={team.teamLogo}
+                          width={100}
+                          height={100}
+                          className="border"
+                          alt="Team Logo"
+                        />
+                      </td>
+                      <td className="px-4 py-auto">{team.teamName}</td>
+                      <td className="px-4 py-auto">{team.teamType}</td>
+                      <td className="px-4 py-auto">
+                        <Modal details={team} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddTeams;
