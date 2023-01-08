979a73848941f99fbb09c079c33f223d75f796f8
sk662516 <sk662516@gmail.com>
2023-01-09 00:23:15 +0530

    Added compression package and used in photocontest

--- pages/photocontest.js
+++ pages/photocontest.js
@@ -21,12 +21,13 @@ import {
 } from "firebase/firestore";
 import moment from "moment";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import imageCompression from "browser-image-compression";
 
 //Sever side props to get the post data from the firestore
 export async function getServerSideProps(context) {
   const postsRef = collection(db, "photocontest");
   const postsSnapshot = await getDocs(postsRef);
-  //Store the data and id in an array
+  // Stores in JSON format where key is id and value is the post data
   const posts = postsSnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
@@ -48,6 +49,11 @@ export async function getServerSideProps(context) {
     });
   }
 
+  // Add a field called isLoading to posts
+  posts.forEach((post) => {
+    post.isLoading = false;
+  });
+
   // Sort the posts in descending order of likes
   posts.sort((a, b) => b.likes - a.likes);
 
@@ -64,6 +70,7 @@ const photocontest = ({ posts }) => {
   const [imageUrl, setImageUrl] = useState(null);
   const [loading, setLoading] = useState(false);
   const [postCount, setPostCount] = useState(3);
+  const [likeUpdate, setLikeUpdate] = useState(false);
 
   //Function to upload the image into firebase storage
   const uploadImage = async (e) => {
@@ -91,8 +98,16 @@ const photocontest = ({ posts }) => {
       `photocontest/${enroll + "-" + timestamp.toString()}`
     );
 
+    // Compress the image
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
+    const compressedFile = await imageCompression(file, options);
+
     // Upload the image to firebase storage
-    const uploadTask = await uploadBytes(storageRef, file);
+    const uploadTask = await uploadBytes(storageRef, compressedFile);
 
     // Get the download url of the image
     const downloadUrl = await getDownloadURL(uploadTask.ref);
@@ -130,7 +145,13 @@ const photocontest = ({ posts }) => {
     }
     const id = post.id;
 
-    setLoading(true);
+    posts.forEach((post) => {
+      if (post.id === id) {
+        post.isLoading = true;
+      }
+    });
+
+    setLikeUpdate(!likeUpdate);
 
     // Increment the likes if the user has not liked the post else decrement the likes
     if (post.liked) {
@@ -152,7 +173,14 @@ const photocontest = ({ posts }) => {
         }
       );
     }
-    setLoading(false);
+
+    posts.forEach((post) => {
+      if (post.id === id) {
+        post.isLoading = false;
+      }
+    });
+
+    setLikeUpdate(!likeUpdate);
 
     // Update the post in posts
     post.liked = !post.liked;
@@ -326,7 +354,7 @@ const photocontest = ({ posts }) => {
                       width={1920}
                       height={1080}
                     />
-                    <div className="flex flex-row justify-between py-4 bg-gray-100 px-4">
+                    <div className="flex flex-row justify-between items-center py-4 bg-gray-100 px-4">
                       <div className="flex justify-center items-center gap-x-3">
                         <div
                           className="cursor-pointer text-xl"
@@ -335,17 +363,39 @@ const photocontest = ({ posts }) => {
                             likePost(post);
                           }}
                         >
-                          {post.liked ? (
-                            <p>
-                              <FcLike />
-                            </p>
-                          ) : (
-                            <p>
-                              <AiOutlineHeart />
-                            </p>
+                          {post.isLoading && (
+                            <div role="status">
+                              <svg
+                                aria-hidden="true"
+                                class="inline w-5 h-5 items-center text-gray-200 animate-spin dark:text-gray-600 fill-red-600 mt-0"
+                                viewBox="0 0 100 101"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                  fill="currentColor"
+                                />
+                                <path
+                                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                  fill="currentFill"
+                                />
+                              </svg>
+                              <span class="sr-only">Loading...</span>
+                            </div>
                           )}
+                          {post.liked
+                            ? !post.isLoading && (
+                                <p>
+                                  <FcLike />
+                                </p>
+                              )
+                            : !post.isLoading && (
+                                <p>
+                                  <AiOutlineHeart />
+                                </p>
+                              )}
                         </div>
-
                         <p>{post.likes} likes</p>
                       </div>
                       <div className="text-sm font-light">
