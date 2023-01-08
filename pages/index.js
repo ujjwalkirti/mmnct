114fdbf0fe3e2600085ce6eb8f265ff519a719a0
sk662516 <sk662516@gmail.com>
2023-01-08 19:17:37 +0530

    Added hidden button for photocontest page

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import TournamentDetails from "../components/TournamentDetails";
 import TournamentHistory from "../components/TournamentHistory";
 import UpcomingMatches from "../components/UpcomingMatches";
 import { listAll, ref, getDownloadURL } from "firebase/storage";
+import Link from "next/link";
 
 export default function Home({ teamList, sponsorImgList }) {
   return (
@@ -32,6 +33,13 @@ export default function Home({ teamList, sponsorImgList }) {
       <TournamentHistory />
       {/* new feature where users can upload there selfies on the matchdays with unique moments which will then be voted by others and one who receives most votes will win */}
       <ImageUploadContest />
+      <div className="flex justify-center mt-8 hidden">
+        <Link href="/photocontest">
+          <button className="bg-[#ffc4b1] text-white font-semibold py-3 px-20 rounded-full">
+            Participate Now!
+          </button>
+        </Link>
+      </div>
       <SponsorCarousel urls={sponsorImgList} />
 
       <Footer />
