4b0597a7383b7ec6c6b8543d9912a77ec08a7c92
Prince <84957374+prince-codes23@users.noreply.github.com>
2023-01-26 04:53:16 +0530

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import HomeCard from "./HomeCard";
+
 // import { SiGooglemaps } from "react-icons/si";
 // import Banner from "./Banner";
 
@@ -38,26 +40,13 @@ function HomePage() {
       <div className="w-full md:w-4/5 md:mx-auto flex flex-col md:flex-row md:items-center md:justify-between">
         {/* graphic and venue along with days to go */}
 
-        <div>
-          <div className=" flex justify-center items-center w-4/5 md:w-[400px] days-counter mx-auto md:mx-0 md:mb-4 md:gap-4 h-[116px] md:h-[150px]">
-            <span className="text-[#F45178] font-[800] text-[96px] md:-[120px] leading-[117px] md:leading-[146.28px] text-center w-1/2 md:w-[35%]">
-              {daysCaluclator()}
-            </span>
-            <div className="text-[#991746]">
-              <p className="font-[700] text-[40px] md:text-[48px] leading-[49px]">
-                DAYS
-              </p>
-              <p className="font-[500] text-[40px] md:text-[48px] leading-[49px]">
-                {" "}
-                TO GO
-              </p>
-            </div>
-          </div>
+         <div>
+          <HomeCard />
           <p className="font-[600] text-[20px] hidden md:flex my-10  md:mx-auto leading-[24.38px]">
             Bring back the Cheers! Bring back the Slogans!
           </p>
         </div>
-        <div className="h-[200px] lg:hidden relative">
+        <div className="h-[200px] ml-2.5 lg:hidden relative">
           <div className={` w-[${screenSize}] h-[230px]`}>
             <Image
               height={250}
@@ -65,10 +54,11 @@ function HomePage() {
               src={`/vector-1.png`}
               alt="ground picture"
               priority
-              className={`z-40 absolute md:relative top-0`}
+              className={`z-40 absolute md:relative top-0 -left-[60px] ml-20`}
             />
           </div>
         </div>
+
 
         {/* the following component will be shown when screen size is larger than 1024 pixels */}
 
