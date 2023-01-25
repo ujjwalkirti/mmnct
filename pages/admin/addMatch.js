2c6c85d286ba70623f7fdfa9a72a297a8c5dfdbd
Prince <84957374+prince-codes23@users.noreply.github.com>
2023-01-25 09:05:54 +0530

    Update addMatch.js

--- pages/admin/addMatch.js
+++ pages/admin/addMatch.js
@@ -104,7 +104,7 @@ const AddMatch = () => {
               <input
                 class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                 name="timeDate"
-                type="datetime-local"
+                type="date"
                 value={formData.timeDate}
                 onChange={handleChange}
                 required
