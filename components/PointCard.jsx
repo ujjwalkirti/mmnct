036a32ceb2bec6f90e5aee1ccb8e603926b8888e
Prince <84957374+prince-codes23@users.noreply.github.com>
2023-01-28 12:12:17 +0530

    Update PointCard.jsx

--- components/PointCard.jsx
+++ components/PointCard.jsx
@@ -19,7 +19,7 @@ const PointCard = (props) => {
             <div className="flex flex-row md:flex-row pt-5 pb-5  justify-center md:justify-evenly items-center my-2 bg-white w-full lg:w-full  min-h-79 lg:h-[90px] xl:h-[150px] rounded-[8px] mx-auto shadow-lg ">
               <div className="w-1/4 flex  items-center justify-center">
                 <p className="text-[15px] xl:text-[25px] lg:text-[20px] font-[600]">
-                  {ind}
+                  {curElem.NRR}
                 </p>
               </div>
               <div className="w-2/3 flex  items-center flex-col md:flex-row">
